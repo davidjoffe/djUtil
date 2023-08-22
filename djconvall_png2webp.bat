f22d1fdcbb5e71ad33d4bc975844140d7acc6341
David Joffe <david@djoffe.com>
2023-08-22 15:04:22 +0200

    Small simple helper to convert all PNG to webp in current folder

---
+++ djconvall_png2webp.bat
@@ -0,0 +1,25 @@
+@echo off
+@echo Small simple helper to convert all .png flies in current folder to webp
+@echo Assumes cwebp is installed and available in path
+
+@REM setlocal enabledelayedexpansion
+@REM 
+@REM for /r %%i in (*.png) do (
+@REM     set filename=%%~ni
+@REM     cwebp -quiet "%%i" -o "!filename!.webp"
+@REM )
+
+@echo off
+setlocal enabledelayedexpansion
+
+rem Set the codepage to UTF-8
+chcp 65001
+
+
+for %%i in (*.png) do (
+    set filename=%%~ni
+    cwebp -quiet "%%i" -o "!filename!.webp"
+
+	@rem Call helper to try copy and preserve original file timestamps e.g. file created date, modified date etc.
+	call copytimestamps.bat "%%i" "!filename!.webp"
+)
