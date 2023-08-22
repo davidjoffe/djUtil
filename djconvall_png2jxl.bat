0b1bc47f3b45a521cf03dbdfff5374104ec5bbfa
davidjoffe <david@djoffe.com>
2023-08-23 00:58:00 +0200

    Add exiftool binary metadata case, and change it so it replaces extension instead of appending png to jxl

--- djconvall_png2jxl.bat
+++ djconvall_png2jxl.bat
@@ -13,6 +13,7 @@ for /r %%f in (*.png) do (
 
 	rem Echo the filename
 	echo Converting %%f...
+	@echo filename only %%~nf
 
 	rem Keep the EXIF data in the JXL file
 	rem and support utf8 filename
@@ -24,23 +25,25 @@ for /r %%f in (*.png) do (
 	rem Convert the PNG file to a JXL file with lossless compression and maximum effort, keeping the EXIF data
 	@rem cjxl -d 0 -e 9 --set-option=exif=keep --set-option=utf8=1   "%%f" "%%f.jxl"
 	@rem cjxl -d 0 -e 1   "%%f" "%%f.jxl" --set-option=exif=copy
-	cjxl -d 0 -e 9   "%%f" "%%f.jxl" 
+	@rem -e compression effort, 9 means spend most time/CPU to get smallest file
+	cjxl -d 0 -e 9   "%%f" "%%~nf.jxl" 
 	
 	@rem exiftool -tagsFromFile "%%i" -all:all "%%i.jxl"
 	@rem exiftool -overwrite_original_in_place -delete_original -r "%%i.jxl	
 	
 	@echo Copying metadata for: %%f
-	exiftool -tagsFromFile "%%f" -all:all "%%f.jxl"
-	exiftool -all -s "%%f" > "%%f_metadata.txt"
-	exiftool -all -s -j "%%f" > "%%f_metadata.json"
+	exiftool -tagsFromFile "%%f" -all:all "%%~nf.jxl"
+	@rem -b for binary data (e.g. Mac screenshots have binary data)
+	exiftool -all -b -s "%%f" > "%%~nf_metadata.txt"
+	exiftool -all -b -s -j "%%f" > "%%~nf_metadata.json"
 	@rem exiftool -overwrite_original_in_place -delete_original -r "%%f_metadata.jxl"
 	@rem Use ImageMagick to copy the metadata 
 	@rem convert "%%f" -set exif:all "%%f.jxl"
 
 	@rem Call my helper to copy and preserve original file timestamps e.g. create date
-	call copytimestamps.bat "%%f" "%%f.jxl"
-	call copytimestamps.bat "%%f" "%%f_metadata.txt"
-	call copytimestamps.bat "%%f" "%%f_metadata.json"
+	call copytimestamps.bat "%%f" "%%~nf.jxl"
+	call copytimestamps.bat "%%f" "%%~nf_metadata.txt"
+	call copytimestamps.bat "%%f" "%%~nf_metadata.json"
 	
 	@echo ONE
 	@rem exiftool
