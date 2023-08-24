28f5ef2835e88406848b32a23f0ea05e1a985667
davidjoffe <david@djoffe.com>
2023-08-24 20:24:04 +0200

    Add recursive subfolder support for batch PNG to JXL script

--- djconvall_png2jxl.bat
+++ djconvall_png2jxl.bat
@@ -1,5 +1,6 @@
 @rem dj2023-08 helper to convert all .png flies in current folder to jxl
 @rem NB this whole thing is meant to be lossless, so e.g. should preserve round-robin all image data, metadata and original file timestamps
+@rem (In theory this should work recursively also on folders, though use with caution - dj2023-08)
 @echo off
 
 @rem set djMETADATA=y
@@ -23,8 +24,11 @@ rem Get a list of all PNG files in the current directory
 for /r %%f in (*.png) do (
 
 	rem Echo the filename
+	echo ---------------
 	echo Converting "%%f" distance %djDISTANCE% to "%%~nf.jxl" ...
-	@echo filename only %%~nf
+	@echo LOG filename only %%~nf, pathonly "%%~dpf", target "%%~dpf%%~nf.jxl"
+	echo ---------------
+
 
 	rem Keep the EXIF data in the JXL file
 	rem and support utf8 filename
@@ -37,39 +41,39 @@ for /r %%f in (*.png) do (
 	@rem cjxl -d 0 -e 9 --set-option=exif=keep --set-option=utf8=1   "%%f" "%%f.jxl"
 	@rem cjxl -d 0 -e 1   "%%f" "%%f.jxl" --set-option=exif=copy
 	@rem -e compression effort, 9 means spend most time/CPU to get smallest file
-	cjxl -d %djDISTANCE% -e 9   "%%f" "%%~nf.jxl" 
+	cjxl -d %djDISTANCE% -e 9   "%%f" "%%~dpf%%~nf.jxl"
 	
 	@rem exiftool -tagsFromFile "%%i" -all:all "%%i.jxl"
 	@rem exiftool -overwrite_original_in_place -delete_original -r "%%i.jxl	
 	
 	if "%djMETADATA%"=="y" (
 		@echo Copying metadata for: %%f
-		exiftool -tagsFromFile "%%f" -all:all "%%~nf.jxl"
+		exiftool -tagsFromFile "%%f" -all:all "%%~dpf%%~nf.jxl"
 		@rem -b for binary data (e.g. Mac screenshots have binary data)
 		if "%djMETADATA_TEXT%"=="y" (
-			exiftool -all -b -s "%%f" > "%%~nf_metadata.txt"
+			exiftool -all -b -s "%%f" > "%%~dpf%%~nf_metadata.txt"
 		)
 		if "%djMETADATA_JSON%"=="y" (
-			exiftool -all -b -s -j "%%f" > "%%~nf_metadata.json"
+			exiftool -all -b -s -j "%%f" > "%%~dpf%%~nf_metadata.json"
 		)
 
 		@rem exiftool -overwrite_original_in_place -delete_original -r "%%f_metadata.jxl"
 		@rem Use ImageMagick to copy the metadata 
-		@rem convert "%%f" -set exif:all "%%f.jxl"
+		@rem convert "%%f" -set exif:all "%%~dpf%%f.jxl"
 	)
 
 	@rem Call my helper to copy and preserve original file timestamps e.g. create date
-	call copytimestamps.bat "%%f" "%%~nf.jxl"
+	call copytimestamps.bat "%%f" "%%~dpf%%~nf.jxl"
 	if "%djMETADATA%"=="y" (
 		if "%djMETADATA_TEXT%"=="y" (
-			call copytimestamps.bat "%%f" "%%~nf_metadata.txt"
+			call copytimestamps.bat "%%f" "%%~dpf%%~nf_metadata.txt"
 		)
 		if "%djMETADATA_JSON%"=="y" (
-			call copytimestamps.bat "%%f" "%%~nf_metadata.json"
+			call copytimestamps.bat "%%f" "%%~dpf%%~nf_metadata.json"
 		)
 	)
 	
-	@echo Done %%f
+	@echo Done "%%f" to "%%~dpf%%~nf.jxl"
 	@rem exiftool
 	@echo ---------------------------------------------------------
 )
