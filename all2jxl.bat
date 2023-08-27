4936d4a2f9f3382fa4f7cfe8072c841cac99eeeb
David Joffe <david@djoffe.com>
2023-08-27 14:02:22 +0200

    Add ability to pass cjxl effort parameter, and small fix to metadata handling for batch PNG to JXL conversion script

--- all2jxl.bat
+++ all2jxl.bat
@@ -1,5 +1,6 @@
 @set djScriptFolder=%~dp0
 @echo djScriptFolder=%djScriptFolder%
 
-echo %1 > all2jxl_log.txt
-@call "%djScriptFolder%\djconvall_png2jxl.bat" %1
+echo %1 %2 > all2jxl_log.txt
+@call "%djScriptFolder%\djconvall_png2jxl.bat" %1 %2
+
